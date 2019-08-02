d3e44f632586ea2fe3d439ea5c9d09c92bf11a74
Alex Standfield <AlexStandfield@gmail.com>
2019-08-02 14:30:56 -0600

    javascript 4 afternoon project complete

--- classes.js
+++ classes.js
@@ -28,7 +28,17 @@
 
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 //Code Here
 
 
@@ -46,7 +56,18 @@
 
   Call your new class Manager
 */
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
+  fire(index){
+    this.reports.splice(index, 1)
+  }
+}
 //Code Here
 
 
@@ -70,7 +91,56 @@
 
   Call your new class ProgressiveManager
 */
-
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age){
+    super(first_name, last_name, email, age)
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hire(employee){
+    super.hire()
+    if(this.reports.length === 0){ 
+      this.title = 'Not a manager';
+    }
+    else if(this.reports.length >= 1 && this.reports.length <= 3){
+      this.title = 'Barely Manager';
+    }
+    else if(this.reports.length >= 4 && this.reports.length <= 10){
+      this.title = 'Mostly Manager';
+    }
+    else if(this.reports.length >= 11 && this.reports.length <= 50){
+      this.title = 'Manager';
+    }
+    else if(this.reports.length >= 51 && this.reports.length <= 100){
+      this.title = 'Manager Plus';
+    }
+    else if(this.reports.length >= 101){
+      this.title = 'Bestest Manager';
+    }
+  }
+fire(employee){
+  super.fire()
+  if(this.reports.length === 0){ 
+    this.title = 'Not a manager';
+  }
+  else if(this.reports.length >= 1 && this.reports.length <= 3){
+    this.title = 'Barely Manager';
+  }
+  else if(this.reports.length >= 4 && this.reports.length <= 10){
+    this.title = 'Mostly Manager';
+  }
+  else if(this.reports.length >= 11 && this.reports.length <= 50){
+    this.title = 'Manager';
+  }
+  else if(this.reports.length >= 51 && this.reports.length <= 100){
+    this.title = 'Manager Plus';
+  }
+  else if(this.reports.length >= 101){
+    this.title = 'Bestest Manager';
+  }
+  this.bonus += 100;
+}
+}
 //Code Here
 
 
